926dc537fb92e5fe3f7bc95a59fe9f9b74a80712
krishashejwal <samikshashejwal.14@gmail.com>
2025-02-03 15:04:57 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,23 @@
+a = 'This is a string'
+print (a)
+L = [1, "a" , "string" , 1+2]
+print (L)
+L.append(6) 
+print (L)
+L.pop()
+print (L)
+print (L[1])
+tup = (1, "a", "string", 1+2)
+print(tup)
+print(tup[1])
+d = {1: 'sam', 2: 'krisha', 3: 'sana'}
+print(d)
+d1 = {1: 'Game', 2: 'of', 3: 'Thrones'}
+print(d1)
+d = { "name": "Alice", 1: "borderland", (1, 2): [1,2,4] }
+print(d["name"])
+d = {1: 'Game', 2: 'of', 3: 'Thrones'}
+d["age"] = 22
+d[1] = "Python dict"
+print(d)
+
